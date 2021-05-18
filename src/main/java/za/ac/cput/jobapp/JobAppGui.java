d5b895833467b9077e3170905763e096846f0ec5
Abel Kabeya <abelkofficial@gmail.com>
2021-05-18 20:50:10 +0200

    Added Title bar and components

--- src/main/java/za/ac/cput/jobapp/JobAppGui.java
+++ src/main/java/za/ac/cput/jobapp/JobAppGui.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package za.ac.cput.jobapp;
-
 /**
  *
  * @author AbelK
  */
-public class JobAppGui {
-    
+package za.ac.cput.jobapp;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class JobAppGui extends JFrame {
+
+    private JPanel panelNorth;
+    private JLabel lblTitle;
+
+    private Font ft1, ft2;
+
+    public JobAppGui() {
+
+        ft1 = new Font("Arial", Font.BOLD | Font.ITALIC, 16);
+        ft2 = new Font("Arial", Font.BOLD, 14);
+
+        panelNorth = new JPanel();
+        lblTitle = new JLabel("LinkUp Job App", SwingConstants.CENTER);
+
+    }
+
+    public void setGUI() {
+
+        panelNorth.setLayout(new GridLayout(1, 1));
+        panelNorth.add(lblTitle);
+
+        this.add(panelNorth, BorderLayout.NORTH);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+
+        new JobAppGui().setGUI();
+
+    }
+
 }
