9bd975d93091aeeed7e67c2c5b1f98f205395309
Abel Kabeya <abelkofficial@gmail.com>
2021-05-29 10:59:38 +0200

    Added new class for seperate window

---
+++ src/main/java/za/ac/cput/jobapp/RunJobApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.jobapp;
+
+/**
+ *
+ * @author AbelK
+ */
+public class RunJobApp {
+    
+     public static void main(String[] args) {
+
+        new JobAppGui().setGUI();
+      //  new JobAppGui2().setGUI();
+
+    }
+
+    
+    
+    
+}
